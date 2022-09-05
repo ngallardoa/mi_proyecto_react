837e1c9ef9fbff597665c76bc62d7d1341ef73be
Nicolas Gallardo <gallardo.3853@gmail.com>
2022-09-05 15:56:43 -0300

    Creación componente navbar

---
+++ src/Components/Header/Navbar/index.jsx
@@ -0,0 +1,28 @@
+import { HStack, Spacer, Link, Box } from "@chakra-ui/react";
+import Logo from "../../../Images/Logo.jpg";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const Navbar = () => {
+    return(
+        <HStack p={2} bgGradient="linear(to-t, red.100, red.500)">
+            <img src={Logo} alt="" width={80} />
+            <Spacer />
+            <HStack>
+                <Box fontSize={32}>Instituto English Everywhere</Box>
+            </HStack>
+            <Spacer />
+            <HStack>
+                <Link p={2}>Inicio</Link>
+                <Link p={2}>Quiénes somos</Link>
+                <Link p={2}>Nuestros cursos</Link>
+                <Link p={2}>Contacto</Link>
+            </HStack>
+            <HStack>
+                <HamburgerIcon m={2} />
+            </HStack>
+        </HStack>
+    )
+}
+
+export { Navbar }
+

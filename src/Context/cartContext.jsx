d4f47ed4b8f70c3fc587c0e20bd0212ef60bb2b8
Nicolas Gallardo <gallardo.3853@gmail.com>
2022-09-30 11:58:32 -0300

    Adición context del carrito

--- src/Context/cartContext.jsx
+++ src/Context/cartContext.jsx
@@ -1,13 +1,46 @@
 import { createContext, useState, useContext } from "react";
 
 const CartContext = createContext([]);
-export const useCartContext = () => useContext(CartContext)
+export const useCartContext = () => useContext(CartContext);
 
 export function CartContextProvider({ children }) {
 
+    const [cartList, setCartList] = useState([]);
+    const isInCart = (id) => cartList.find(product => product.id === id);
+    const addToCart = (item, quantity) => {
+        if (isInCart(item.id)) {
+            const newCart = cartList.map(product => {
+                if (product.id === item.id) {
+                    const newQuantity = product.quantity + quantity;
+                    return {...product, quantity: newQuantity}
+                }
+                else {
+                    return product
+                }
+            })
+            setCartList(newCart)
+        }
+        else {
+            const newProduct = {...item, quantity: quantity }
+            setCartList([...cartList, newProduct])
+        }
+    }
+
+    const removeProduct = (id) => setCartList(cartList.filter(product => product.id != id));
+    const cleanCart = () => setCartList([]);
+    const totalPrice = () => {
+        return cartList.reduce((acc, product) => acc += (product.precio * product.quantity), 0)
+    }
+    const totalQuantity = () => {
+        return cartList.reduce((acc, product) => acc += product.quantity, 0)
+    }
+
     return(
         <CartContext.Provider value={{
-
+            addToCart,
+            removeProduct,
+            cleanCart,
+            totalPrice
         }}>
             {children}
         </CartContext.Provider>
